35f9ff49cf19367f6cd4d16f7716fea2b6936f85
Benjamin Marsh <coding@sparty182020.anonaddy.com>
2022-12-24 13:49:17 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,125 +1,122 @@
+//@ts-nocheck
 function getHistory() {
-    return document.getElementById("history-value").innerText;
-
+    return document.getElementById("history-value").innerText | undefined;
 }
+
 function printHIstory(num) {
-    document.getElementById("history-value").innerText = num;
+    try {
+        document.getElementById("history-value").innerText = num;
+    } catch (err) {
+        throw err
+    }
 }
 
 function getOutput() {
-    return document.getElementById("output-value").innerText;
+    return document.getElementById("output-value").innerText | undefined;
 }
-function printOutput(num) {
 
-    if (num == "") {
-        document.getElementById("output-value").innerText = num;
-    } else {
-        document.getElementById("output-value").innerText = getFormativeNum(num);
-    }
+function printOutput(num) {
+    document.getElementById("output-value").innerText =
+        (num == "") ?
+            num :
+            getFormativeNum(num);
 }
 
 function getFormativeNum(num) {
-
-    if (num == '-') {
-        return "";
-    }
-
-    var n = Number(num);
-    var value = n.toLocaleString('en');
-    return value;
+    return (num == "-") ? "" : Number(num).toLocaleString('en');
 }
 
 function reverseNumberFormat(num) {
-    return Number(num.replace(/,/g, ''));
+    return Number(String(num).replace(/,/g, ''));
 }
 
 // printOutput("99999");
 
 // alert(reverseNumberFormat( getOutput() ));
 
-var oprators = document.getElementsByClassName("operator");
+const operators = document.getElementsByClassName("operator");
 
-for (var i = 0; i < oprators.length; i++) {
-    oprators[i].addEventListener('click', function () {
-        if (this.id == "clear") {
-            printHIstory("");
-            printOutput("");
-        }
-        else if (this.id == "backspace") {
-            var output = reverseNumberFormat(getOutput()).toString();
-            if (output) {
-                output = output.substr(0, output.length - 1);
-                printOutput(output);
-            }
-        }
-        else if(this.id=="!"){
-            var output = getOutput();
-            var history = getHistory();
-            console.log(output);
-            console.log(history);
-            let n= output;
-            console.log("v");
-            let answer = 1;
-            if (n == 0 || n == 1){
-                answer =1;
-            }
-            else{
-                for(var i = n; i >= 1; i--){
-                    answer = answer * i;
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function () {
+        switch (this.id) {
+            case "clear":
+                printHIstory("");
+                printOutput("");
+                break;
+            case "backspace":
+                let output = reverseNumberFormat(getOutput()).toString();
+                if (output) {
+                    output = output.substr(0, output.length - 1);
+                    printOutput(output);
                 }
-                output =answer;
-                history=history+output;
-                var res = eval(history);
+                break;
+            case "sign":
+                let output = getOutput();
+                let history = getHistory();
+                history += output;
+                let res = eval(history);
+                res *= -1;
                 printOutput(res);
                 printHIstory("");
-            }  
-        }
-        else if(this.id=="sign"){
-            var output = getOutput();
-            var history = getHistory();
-            history=history+output;
-            var res = eval(history);
-            res=res*-1;
-            printOutput(res);
-            printHIstory(""); 
-        }  
-        else {
-            var output = getOutput();
-            var history = getHistory();
-
-            if (output == "" && history != "") {
-                if (isNaN(history[history.length - 1])) {
-                    history = history.substr(0, history.length - 1);
+                break;
+            case "!":
+                let output = getOutput();
+                let history = getHistory();
+                console.log(output);
+                console.log(history);
+                let n = output;
+                console.log("v");
+                let answer = 1;
+                if (n == 0 || n == 1) {
+                    answer = 1;
                 }
-            }
-
-            if (output != "" || history != "") {
-                output = output === "" ? output : reverseNumberFormat(output);
-                history = history + output;
-
-                if (this.id == "=") {
-                    var res = eval(history);
+                else {
+                    for (let i = n; i >= 1; i--) answer *= i;
+                    output = answer;
+                    history += output;
+                    let res = eval(history);
                     printOutput(res);
                     printHIstory("");
                 }
-                else {
-                    history = history + this.id;
-                    printHIstory(history);
-                    printOutput("");
+                break;
+            default:
+                let output = getOutput();
+                let history = getHistory();
+
+                if (output == "" && history != "") {
+                    if (isNaN(history[history.length - 1])) {
+                        history = history.substr(0, history.length - 1);
+                    }
                 }
-            }
+
+                if (output != "" || history != "") {
+                    output = (output === "") ? output : reverseNumberFormat(output);
+                    history += output;
+
+                    if (this.id == "=") {
+                        let res = eval(history);
+                        printOutput(res);
+                        printHIstory("");
+                    }
+                    else {
+                        history += this.id;
+                        printHIstory(history);
+                        printOutput("");
+                    }
+                }
+                break;
         }
     })
 }
 
-var numbers = document.getElementsByClassName("number");
+const numbers = document.getElementsByClassName("number");
 
-for (var i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', function () {
-        var output = reverseNumberFormat(getOutput());
+        let output = reverseNumberFormat(getOutput());
         console.log(output);
         if (output != NaN) {
-            output = output + this.id;
+            output += this.id;
             printOutput(output);
         }
     });
@@ -127,38 +124,30 @@ for (var i = 0; i < numbers.length; i++) {
 
 
 // For Darkmode
-var darkmode = document.getElementById("toggle-icon");
-
-darkmode.onclick = function () {
+document.getElementById("toggle-icon").onclick = function () {
     document.body.classList.toggle("dark-theme");
-    var icon = document.getElementById("toggle-icon");
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "img/sun.png";
-    }
-    else {
-        icon.src = "img/moon.png";
-    }
-
+    document.getElementById("toggle-icon").src =
+        (document.body.classList.contains("dark-theme")) ?
+            "img/sun.png" :
+            "img/moon.png";
 }
 
-
-var play=true;
-var count=1;
-document.querySelector("#mute").addEventListener('click',function(){
-    if(count%2){
-        play=false;
-        mute.innerText='🔇';
+let play = true;
+let count = 1;
+document.querySelector("#mute").addEventListener('click', function () {
+    if (count % 2) {
+        play = false;
+        mute.innerText = '🔇';
     }
-    else{
-       play=true;
-       mute.innerText='🔊';
+    else {
+        play = true;
+        mute.innerText = '🔊';
     }
     count++;
 });
 
-document.querySelector(".keyboard").addEventListener('click', function(){
-    if(play){
-        var audio= new Audio("./Audio/calculatorclick.mp3");
-        audio.play();
+document.querySelector(".keyboard").addEventListener('click', function () {
+    if (play) {
+        new Audio("./Audio/calculatorclick.mp3").play();
     }
 });
